8b03dc509468e9cfec7d073bddde40cce30b5252
JitenderMX <jitender.work.mediax@gmail.com>
2024-12-18 13:28:49 +0530

    rishb small changes

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -23,6 +23,7 @@ function Footer() {
                   <li><a href="https://www.youtube.com/@happeninginagra" target='_blank'><img src={Youtube} alt="" className="w-100" /></a></li>
                   <li><a href="https://www.facebook.com/happeningin.agra" target='_blank'><img src={Facebook} alt="" className="w-100" /></a></li>
                   <li><a href="https://mojapp.in/@happeninginagra?referrer=iOS&h=UXI9jKE-yuCJbn" target='_blank'><img src={Moj} alt="" className="w-100" /></a></li>
+                  
                 </ul>
               </div>
               <p className='mt-5'>
@@ -34,10 +35,14 @@ function Footer() {
             <div className="foot-link">
               <h4>Important Links</h4>
               <ul>
-                <li><a href='/contact-us'><i class="far fa-angle-right"></i> Contact Us</a></li>
+                <li><a href='https://happeninginagra.com/contact-us'><i class="far fa-angle-right"></i> Contact Us</a></li>
+                <li><a href='https://happeninginagra.com/privacy-policy'><i class="far fa-angle-right"></i> Privacy policy</a></li>
+                <li><a href='https://happeninginagra.com/refund-policy'><i class="far fa-angle-right"></i> Refund and cancellation policy</a></li>
+                <li><a href='https://happeninginagra.com/terms-n-conditions'><i class="far fa-angle-right"></i> Terms & conditions</a></li>
+                {/* <li><a href='/contact-us'><i class="far fa-angle-right"></i> Contact Us</a></li>
                 <li><a href='/privacy-policy'><i class="far fa-angle-right"></i> Privacy policy</a></li>
                 <li><a href='/refund-policy'><i class="far fa-angle-right"></i> Refund and cancellation policy</a></li>
-                <li><a href='/terms-n-conditions'><i class="far fa-angle-right"></i> Terms & conditions</a></li>
+                <li><a href='/terms-n-conditions'><i class="far fa-angle-right"></i> Terms & conditions</a></li> */}
               </ul>
             </div>
           </div>
