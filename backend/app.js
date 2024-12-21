f4bef00deaff31bdde50247ce2f48f77c2589209
Jitendra Khatri <jkhatri6@gmail.com>
2024-12-21 22:34:45 +0530

    Enabled Cashfree analytics

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({
 }));
 
 
-
+Cashfree.XEnableErrorAnalytics = true;
 Cashfree.XClientId = process.env.CLIENT_ID;
 Cashfree.XClientSecret = process.env.CLIENT_SECRET;
 Cashfree.XEnvironment = Cashfree.Environment.PRODUCTION;
